0d3d67aea34d58e9bf7fa78e4ccff4b4b1f43dac
flipstar1 <flip014@mail.ru>
2023-01-13 16:13:35 +0300

    add change type of menu from qt to pygame

--- menu.py
+++ menu.py
@@ -10,18 +10,36 @@ WIN_WIDTH = 800  # Ширина создаваемого окна
 WIN_HEIGHT = 640  # Высота
 DISPLAY = (WIN_WIDTH, WIN_HEIGHT)
 
+
 def menu():
     pygame.init()
     screen = pygame.display.set_mode(DISPLAY)  # Создаем окошко
     pygame.display.set_caption("Super Mario Boy")  # Пишем в шапку
     bg = pygame.image.load('Меню-1.png')  # добавляем фоновое изображение
-    while True:
+    continue_button = pygame.image.load('blocks\dieBlock.png')
+    continue_button_rect = continue_button.get_rect(topleft=(280, 250))
+    chose_level = False
+    while not chose_level:
         screen.blit(bg, (0, 0))
-        pygame.display.update()
+        screen.blit(continue_button, continue_button_rect)
+        mouse = pygame.mouse.get_pos()
+        if continue_button_rect.collidepoint(mouse) and pygame.mouse.get_pressed()[0]:
+            chose_level = True
         for e in pygame.event.get():  # Обрабатываем события
             if e.type == pygame.constants.QUIT:
                 raise SystemExit
-
+        pygame.display.update()
+    while chose_level:
+        screen.fill((125, 42, 238))
+        #screen.blit(bg, (0, 0))
+        #screen.blit(continue_button, continue_button_rect)
+        mouse = pygame.mouse.get_pos()
+        if continue_button_rect.collidepoint(mouse) and pygame.mouse.get_pressed()[0]:
+            chose_level = True
+        for e in pygame.event.get():  # Обрабатываем события
+            if e.type == pygame.constants.QUIT:
+                raise SystemExit
+        pygame.display.update()
 
 
 def except_hook(cls, exception, traceback):
@@ -30,6 +48,3 @@ def except_hook(cls, exception, traceback):
 
 if __name__ == "__main__":
     menu()
-
-
-
